cc8759a3bbaeb3c96e50d8e66e5d2bed1fa3d684
Salman Ahmad <m.salman86@gmail.com>
2019-02-01 09:49:38 -0500

    solved find perfect squares between two numbers

--- math/perfect_squares.go
+++ math/perfect_squares.go
@@ -8,3 +8,25 @@ func countPerfectSquares(x, y int64) int {
   result := math.Floor(math.Sqrt(float64(y))) - math.Ceil(math.Sqrt(float64(x))) + 1
   return int(result)
 }
+
+// Problem: Find perfect squares between two numbers
+// assume the numbers will always be positive
+
+// solve using (n + 1)^2 = n^2 + 2n + 1
+// where first n = int(sqrt(x)) and iterate until n < k
+func findPerfectSquares(x, y int64) []int64 {
+  n := int64(math.Sqrt(float64(x)))
+  k := int64(math.Sqrt(float64(y)))
+  var squares []int64
+  
+  // exclude y
+  if y-k*k == 0 {
+    k--
+  }
+  
+  for square := n * n; n < k; n++ {
+    square += 2*n + 1
+    squares = append(squares, square)
+  }
+  return squares
+}
--- math/perfect_squares_test.go
+++ math/perfect_squares_test.go
@@ -1,10 +1,11 @@
 package math
 
 import (
+  "reflect"
   "testing"
 )
 
-func TestCountSquares(t *testing.T) {
+func TestCountPerfectSquares(t *testing.T) {
   expected := 2
   actual := countPerfectSquares(2, 10)
   if actual != expected {
@@ -12,7 +13,7 @@ func TestCountSquares(t *testing.T) {
   }
 }
 
-func TestCountSquaresSameNumbers(t *testing.T) {
+func TestCountPerfectSquares_SameNumbers(t *testing.T) {
   expected := 0
   actual := countPerfectSquares(2, 2)
   if actual != expected {
@@ -20,10 +21,34 @@ func TestCountSquaresSameNumbers(t *testing.T) {
   }
 }
 
-func TestCountSquaresSquareNumbers(t *testing.T) {
+func TestCountPerfectSquares_SquaredNumbers(t *testing.T) {
   expected := 0
   actual := countPerfectSquares(4, 9)
   if actual != expected {
     t.Errorf("Got: %d, wanted: %d\n", actual, expected)
+  }
+}
+
+func TestFindPerfectSquares(t *testing.T) {
+  expected := []int64{4, 9, 16, 25}
+  actual := findPerfectSquares(3, 30)
+  if !reflect.DeepEqual(actual, expected) {
+    t.Errorf("Got: %v, wanted: %v\n", actual, expected)
+  }
+}
+
+func TestFindPerfectSquares_SameNumbers(t *testing.T) {
+  var expected []int64
+  actual := findPerfectSquares(3, 3)
+  if actual != nil || len(actual) > 0 {
+    t.Errorf("Got: %v, wanted: %v\n", actual, expected)
+  }
+}
+
+func TestFindPerfectSquares_SquaredNumbers(t *testing.T) {
+  expected := []int64{9, 16}
+  actual := findPerfectSquares(4, 25)
+  if !reflect.DeepEqual(actual, expected) {
+    t.Errorf("Got: %v, wanted: %v\n", actual, expected)
   }
 }
